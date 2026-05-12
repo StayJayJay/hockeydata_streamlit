39d06b5bc4254737f97534c6eb1347034cddc369
Jakub Jirmann <jirmannjakub@gmail.com>
2026-05-12 10:13:45 +0200

    v0.0.1

--- backend/database.py
+++ backend/database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+#local
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "hockey.db")
 
